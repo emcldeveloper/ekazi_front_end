d8d7f839d8d95c866c6f6a9b8d5e665039839036
Ibrahim Herith <ibrahimherith19@gmail.com>
2026-02-11 12:24:28 +0300

    Changes on fonts

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '@fortawesome/fontawesome-free/css/all.min.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <App />
+  <div className="font-Sen">
+    <App />
+  </div>,
 );
 
 reportWebVitals();
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      colors:{
-        primary:"#2E58A6",
-        secondary:"#D36314",
-        darkShadow:'#3C414F',
-        dark:'#111729'
+      colors: {
+        primary: "#2E58A6",
+        secondary: "#D36314",
+        darkShadow: "#3C414F",
+        dark: "#111729",
       },
       animation: {
         marquee: "marquee 10s linear infinite",
@@ -21,14 +19,9 @@ module.exports = {
         },
       },
     },
-    fontFamily:{
-      aeonik:[
-        'Aeonik'
-      ]
-    }
+    fontFamily: {
+      Sen: ["Sen", "sans-serif"],
+    },
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-  ],
-}
- 
+  plugins: [require("@tailwindcss/forms")],
+};
